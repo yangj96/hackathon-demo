ff3fb508700adfb4f2700704e9831d5cfd6381a2
yangj96 <yangj_96@126.com>
2019-09-27 18:33:22 +0900

    homepage init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,55 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <a-layout id="components-layout-demo-top" class="layout">
+
+    <a-layout-header>
+      <a-row>
+        <a-col :span="8">
+          <a-menu
+                  theme="dark"
+                  mode="horizontal"
+                  :defaultSelectedKeys="['2']"
+                  :style="{ lineHeight: '64px' }"
+          >
+            <a-menu-item key="1">Trending Network</a-menu-item>
+          </a-menu>
+        </a-col>
+        <a-col :span="8"></a-col>
+        <a-col :span="8">
+          <a-input-search
+                  placeholder="Github Account"
+                  style="width: 200px"
+                  @search="onSearch"
+          />
+        </a-col>
+      </a-row>
+    </a-layout-header>
+    <a-layout-content style="padding: 0 50px">
+      <a-breadcrumb style="margin: 16px 0">
+      </a-breadcrumb>
+      <div :style="{ background: '#fff', padding: '24px', minHeight: '280px' }">
+        
+
+      </div>
+    </a-layout-content>
+    <a-layout-footer style="text-align: center">
+      Ant Design ©2018 Created by Ant UED
+    </a-layout-footer>
+  </a-layout>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+  name: 'app'
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #components-layout-demo-top .logo {
+    width: 120px;
+    height: 31px;
+    background: rgba(255,255,255,.2);
+    margin: 16px 24px 16px 0;
+    float: left;
+  }
 </style>
